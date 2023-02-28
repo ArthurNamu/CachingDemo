7c9e082ad64c7096ec98284615a7bd02bb185393
Arthur Namu <ArthurN@hollywoodbets.net>
2023-02-28 22:37:02 +0300

    Uploaded Redis demo

---
+++ CachingWithReddis/Extensions/DistributedCacheExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Runtime.Serialization.Formatters;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CachingWithReddis.Extensions
+{
+    public static class DistributedCacheExtensions
+    {
+        public static async Task SetRecordAsync<T>(this IDistributedCache cache,
+            string recordId,
+            T data,
+            TimeSpan? absoluteExpireTime = null,
+            TimeSpan? unusedExpiteTime = null)
+        {
+            var options = new DistributedCacheEntryOptions();
+            options.AbsoluteExpirationRelativeToNow = absoluteExpireTime ?? TimeSpan.FromSeconds(60);
+            options.SlidingExpiration = unusedExpiteTime;
+
+            var jsonData = JsonSerializer.Serialize(data);
+
+            await cache.SetStringAsync(recordId, jsonData, options);
+        }
+
+        public static async Task<T> GetRecordAsync<T>(this IDistributedCache cache, string recordId)
+        {
+            var jsonData = await cache.GetStringAsync(recordId);
+
+            if(jsonData is null)
+            {
+                return default(T);
+            }
+
+            return JsonSerializer.Deserialize<T>(jsonData);
+        }
+    }
+}
